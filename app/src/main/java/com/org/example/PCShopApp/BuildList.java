fbd6136626c17e1eaaf086c6323788e042837158
Cryptic. <sanjuanrj40@gmail.com>
2025-05-04 15:19:24 +0800

    Added Flatlaf on Buildlistframe

--- app/src/main/java/com/org/example/PCShopApp/BuildList.java
+++ app/src/main/java/com/org/example/PCShopApp/BuildList.java
@@ -4,6 +4,8 @@
  */
 package com.org.example.PCShopApp;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+
 /**
  *
  * @author JORICK
@@ -284,6 +286,9 @@ public class BuildList extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
+        FlatLaf.registerCustomDefaultsSource("com.org.example.PCShopApp");
+        FlatDarkLaf.setup();
+
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
